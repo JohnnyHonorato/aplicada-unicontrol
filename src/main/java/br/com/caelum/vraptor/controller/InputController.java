a5cbfcdb60382109304f78d4537426141d0504ae
Herbert Cordeiro <herbert.cordeiro@dce.ufpb.br>
2018-05-12 19:36:12 -0300

    [Front] Cadastro de entradas

---
+++ src/main/java/br/com/caelum/vraptor/controller/InputController.java
@@ -0,0 +1,15 @@
+package br.com.caelum.vraptor.controller;
+
+import java.util.Locale.Category;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class InputController {
+
+	@Get
+	public void formulario() {
+		
+	}
+}
