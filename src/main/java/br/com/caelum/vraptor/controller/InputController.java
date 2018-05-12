dce435f3a45d2c2f5c1dbe2a015ed64840cb517d
JohnnyHonorato <johnny.menezes@dce.ufpb.br>
2018-05-12 19:46:43 -0300

    ajuste de controller

--- src/main/java/br/com/caelum/vraptor/controller/InputController.java
+++ src/main/java/br/com/caelum/vraptor/controller/InputController.java
@@ -2,14 +2,35 @@ package br.com.caelum.vraptor.controller;
 
 import java.util.Locale.Category;
 
+import javax.inject.Inject;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.DAO.InputDAO;
+import br.com.caelum.vraptor.model.Input;
 
 @Controller
 public class InputController {
 
+	private InputDAO bd = new InputDAO();
+	@Inject
+	private Result result;
+	
+	@Post
+	public void insert(Input input) {
+		bd.insert(input);
+		result.redirectTo(this).confirmacaoCad();
+	}
+	
 	@Get
 	public void formulario() {
 		
+	}
+	
+	@Get
+	public void confirmacaoCad() {
+
 	}
 }
