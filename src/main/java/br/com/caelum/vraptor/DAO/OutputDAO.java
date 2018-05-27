3ab3554454f9a187acc424f56d0088a31b19b8fb
JohnnyHonorato <johnny.menezes@dce.ufpb.br>
2018-05-27 11:23:50 -0300

    [back] DAO

--- src/main/java/br/com/caelum/vraptor/DAO/OutputDAO.java
+++ src/main/java/br/com/caelum/vraptor/DAO/OutputDAO.java
@@ -8,10 +8,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.com.caelum.vraptor.model.Input;
-import br.com.caelum.vraptor.model.Product;
+import br.com.caelum.vraptor.model.OutPut;
 
-public class InputDAO implements Serializable {
+public class OutputDAO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private EntityManagerFactory factory = Persistence.createEntityManagerFactory("nn");
@@ -19,30 +18,30 @@ public class InputDAO implements Serializable {
 
 	private ProductDAO productBD = new ProductDAO();
 
-	public void insert(Input input) {
-		if (input.getAmount() <= 0)
+	public void insert(OutPut output) {
+		if (output.getAmount() <= 0)
 			throw new IllegalArgumentException();
-		productBD.updateAmount(input.getAmount(), input.getId_product());
+		productBD.updateAmount((output.getAmount() * -1), output.getId_product());
 		manager.getTransaction().begin();
-		input.setData(Calendar.getInstance().getTime());
-		manager.merge(input);
+		output.setData(Calendar.getInstance().getTime());
+		manager.merge(output);
 		manager.getTransaction().commit();
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<Input> getAll() {
-		return manager.createQuery("FROM " + Input.class.getName()).getResultList();
+	public List<OutPut> getAll() {
+		return manager.createQuery("FROM " + OutPut.class.getName()).getResultList();
 	}
 
-	public Input getById(long id) {
-		return manager.find(Input.class, id);
+	public OutPut getById(long id) {
+		return manager.find(OutPut.class, id);
 	}
 
-	public void remove(Input inputD) {
-		Input input = manager.find(Input.class, inputD.getId());
-		productBD.updateAmount((input.getAmount() * -1), input.getId_product());
+	public void remove(OutPut outputD) {
+		OutPut output = manager.find(OutPut.class, outputD.getId());
+		productBD.updateAmount(output.getAmount(), output.getId_product());
 		manager.getTransaction().begin();
-		manager.remove(input);
+		manager.remove(output);
 		manager.getTransaction().commit();
 	}
 
--- src/main/java/br/com/caelum/vraptor/DAO/Teste.java
+++ src/main/java/br/com/caelum/vraptor/DAO/Teste.java
@@ -1,20 +1,16 @@
 package br.com.caelum.vraptor.DAO;
 
-import br.com.caelum.vraptor.model.Category;
-import br.com.caelum.vraptor.model.Input;
+import br.com.caelum.vraptor.model.OutPut;
 import br.com.caelum.vraptor.model.Product;
 
 public class Teste {
 
 	public static void main(String[] args) {
 
-		InputDAO d = new InputDAO();
-
-		Input d2 = new Input();
-		d2.setAmount(10);
-		d2.setId_product(1);
-
-	//	d.remove(1);
-
+		OutPut p4 = new OutPut();
+		p4.setAmount(10);
+		p4.setId_product(1);
+		OutputDAO p2 = new OutputDAO();
+		p2.insert(p4);
 	}
 }
